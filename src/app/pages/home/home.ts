0138d2c17e181d49223e67df8b2d43edeeb70bd4
sacazidal <tw1nkle001@yandex.ru>
2026-01-17 18:07:25 +0700

    replace component in home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { HeroComponent } from '../../components/hero/hero';
 import { AdvantageComponent } from '../../components/advantage/advantage';
 import { InfoComponent } from '../../components/info/info';
 import { CardsComponent } from '../../components/cards/cards';
+import { CarsPreviewComponent } from '../../components/cars-preview/cars-preview';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AdvantageComponent, InfoComponent, CardsComponent],
+  imports: [HeroComponent, AdvantageComponent, InfoComponent, CardsComponent, CarsPreviewComponent],
   templateUrl: './home.html',
   styleUrl: './home.less',
 })
