1fbf349e3de4cebacf02a5402f259ce09b8883ac
sacazidal <tw1nkle001@yandex.ru>
2026-01-17 17:54:25 +0700

    created a card component for cards

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero';
 import { AdvantageComponent } from '../../components/advantage/advantage';
 import { InfoComponent } from '../../components/info/info';
+import { CardsComponent } from '../../components/cards/cards';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AdvantageComponent, InfoComponent],
+  imports: [HeroComponent, AdvantageComponent, InfoComponent, CardsComponent],
   templateUrl: './home.html',
   styleUrl: './home.less',
 })
